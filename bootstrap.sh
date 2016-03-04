7ee381d683736b361ce6ee0a02917b645c361efa
Olivier Ligot <olivier.ligot@groups.be>
2016-03-04 12:03:53 +0100

    bootstrap.sh now install oh-my-zsh, git-radar and diff-so-fancy

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 pwd=`pwd`
 
+# Dotfiles
 for i in `ls -A src`; do
     basename=`basename $i`
     rm -rf $HOME/$basename
@@ -10,3 +11,20 @@ for i in `ls -A src`; do
     echo "Linking $target to $name"
     ln -s $target $name
 done
+
+# Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+fi
+rm -f $HOME/.oh-my-zsh/themes/oli.zsh-theme
+ln -s $pwd/oli.zsh-theme $HOME/.oh-my-zsh/themes
+
+# Git
+if [ ! -d "$HOME/.git-radar" ]; then
+    git clone https://github.com/michaeldfallen/git-radar.git $HOME/.git-radar
+fi
+if [ ! -d "$HOME/.diff-so-fancy" ]; then
+    git clone https://github.com/so-fancy/diff-so-fancy.git $HOME/.diff-so-fancy
+    mkdir -p $HOME/bin
+    ln -s $HOME/.diff-so-fancy/diff-so-fancy $HOME/bin/diff-so-fancy
+fi
--- oli.zsh-theme
+++ oli.zsh-theme
@@ -1,4 +1,4 @@
 local ret_status="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ %s)"
 PROMPT='${ret_status}%{$fg_bold[green]%}%p %{$fg[cyan]%}%c% %{$reset_color%}'
-PROMPT="$PROMPT\$(~/src/git-radar/git-radar --zsh --fetch) "
+PROMPT="$PROMPT\$(~/.git-radar/git-radar --zsh --fetch) "
 RPROMPT="%F{green}%~%f"
